3a3134a1500d3afd902304824062369cb7df34ba
deblur99 <deblur99@kakao.com>
2023-08-09 13:13:16 +0900

    Initial Commit

---
+++ GoogleAuthExample/GoogleAuthExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAuthExampleApp.swift
+//  GoogleAuthExample
+//
+//  Created by 한현민 on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
