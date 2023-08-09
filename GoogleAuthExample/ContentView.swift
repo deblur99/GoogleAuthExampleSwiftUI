c557b47cd6422b834ba772f24f1eb0ebac6c6bc3
deblur99 <deblur99@kakao.com>
2023-08-09 15:05:12 +0900

    Initial Commit

--- GoogleAuthExample/ContentView.swift
+++ GoogleAuthExample/ContentView.swift
@@ -5,15 +5,71 @@
 //  Created by 한현민 on 2023/08/09.
 //
 
+import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
+import GoogleSignInSwift // SUI
 import SwiftUI
 
+@MainActor
+final class AuthenticationViewModel: ObservableObject {
+    @Published var credential: AuthCredential? = nil
+    
+    func signInGoogle() async throws {
+        // 로그인 버튼 만들기
+        guard let topVC = Utilities.shared.topViewController() else {
+            throw URLError(.cannotFindHost)
+        }
+        
+        // Client ID 생성 및 로그인 설정
+        guard let clientID = FirebaseApp.app()?.options.clientID else {
+            return
+        }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // 로그인 인증
+        let gidSignInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        
+        // 토큰 생성
+        guard let idToken = gidSignInResult.user.idToken?.tokenString else {
+            throw URLError(.badServerResponse)
+        }
+        let accessToken = gidSignInResult.user.accessToken.tokenString
+//        gidSignInResult.user.
+        
+        // Credential 생성
+        credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        print(credential?.description ?? nil)
+    }
+}
+
+//extension AuthenticationManager
+
 struct ContentView: View {
+    @StateObject private var viewModel = AuthenticationViewModel()
+    @Binding var showSignInView: Bool
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .light, style: .wide, state: .normal)) {
+                Task {
+                    do {
+                        try await viewModel.signInGoogle()
+
+                    } catch {
+                        debugPrint("error")
+                    }
+                }
+            }
+            
+            NavigationLink {
+                NavigationStack {
+                    Text(viewModel.credential?.description ?? "")
+                }
+            } label: {
+                Text("Inner View")
+            }
         }
         .padding()
     }
@@ -21,6 +77,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(showSignInView: .constant(false))
     }
 }
--- GoogleAuthExample/GoogleAuthExampleApp.swift
+++ GoogleAuthExample/GoogleAuthExampleApp.swift
@@ -5,13 +5,33 @@
 //  Created by 한현민 on 2023/08/09.
 //
 
+import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
+
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+}
 
 @main
 struct GoogleAuthExampleApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(showSignInView: .constant(false))
         }
     }
 }
